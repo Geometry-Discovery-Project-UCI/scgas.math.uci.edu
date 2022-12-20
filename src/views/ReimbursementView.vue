92672ae7a4cae91504fe32ab70c163c027a42878
Kiyo5hi <44930252+Kiyo5hi@users.noreply.github.com>
2022-12-20 01:11:19 +0000

    chore(*): organized pdfs

--- src/views/ReimbursementView.vue
+++ src/views/ReimbursementView.vue
@@ -1,11 +1,13 @@
 <template>
   <strong>Instructions: </strong> please fill in all required forms and send
-  them back to Ingrid Richey (irichey@uci.edu).
+  them back to Ingrid Richey (<a href="mailto:irichey@uci.edu"
+    >irichey@uci.edu</a
+  >).
   <ol>
     <li v-for="document in documents" :key="document">
       <div class="reimbursement-document hover-shadow">
         <a :href="document" target="_blank">{{
-          document.split(".")[0].replaceAll("_", " ")
+          document.split("/")[1].split(".")[0].replaceAll("_", " ")
         }}</a>
       </div>
     </li>
@@ -25,16 +27,12 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import config from "@/config.json";
 
 export default defineComponent({
   setup: () => {
     return {
-      documents: [
-        "travelers_guidelines_2022.pdf",
-        "certificate_of_academic_activity.pdf",
-        "travel_reimbursement_form.pdf",
-        "mileage_log_sheet.pdf",
-      ],
+      documents: config.reimbursementDocuments,
     };
   },
 });
